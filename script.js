4b3d1a7ecf96a8c2ee51c5a6526c78260f5ca398
Yan Kolev <y.kolev11@gmail.com>
2024-08-27 10:02:44 +0200

    Module Pattern + CommonJS Modules

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ console.log(price, tq);
 
 console.log('Importing module');
 
+import shoppingCart from "./shoppingCart.js";
 /*
 // we can also import all the export of a module at the same time 
 // below it will create an object containing everything that is exported, and needs to start with capital letter(naming sorta like a class)
@@ -76,3 +77,71 @@ console.log(lastPost2);
 //top level await- if one module imports a module which has a top level await, then the imporing module, 
 // will wait for the imported to finish the blocking code. 
 
+
+
+//MODULE PATTERN USED BEFORE, STILL AROUND
+
+
+//main goal is to encapsulate functionality to have private date and to expose public API, 
+//best way to achieve is function. 
+
+//IIFE- immediately invoked function expression- can call it once, and we do not have to call it separatly
+const ShoppingCart2 = (function(){
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity){
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} addded to cart`)
+    }
+
+    const orderStock = function (product, quantity){
+    
+        console.log(`${quantity} ${product} ordered from supplier`)
+    }
+
+    //in this return object will be exposed to outside
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+
+
+})()
+//only purpose of the function is to create a new scope and return the data once
+//all of the data is private, because its inside of the scope of the function, now we need to return it to have a public api- retun the stuff u want to make public
+
+ShoppingCart2.addToCart('apple', 4)
+ShoppingCart2.addToCart('pizza', 2)
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); // it will be undefined as the data is private and only visible to the module
+
+/* 
+Since the IFFE is executed only once, how can we be able to manipulate the cart after its already executed? 
+Closures- they allow a function to have access to the variables that were present at its birthplace,
+the add to cart function can acess the cart as its was at its birthplace
+*/
+
+// CommonJS modules
+
+/*
+NPM was only intended for Node, hence it uses commonJS
+
+
+
+
+export.addToCart =function (product, quantity){
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} addded to cart`)
+}
+//it will not work in browser but in nodeJS,
+
+//to import.
+const {addToCart} = require = (''./shoppingCart.js')
+
+*/
